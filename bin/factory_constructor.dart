3c9b5f36a6956d4c074c98a229af9c4121321487
ukasyaaah <ukasyaaah@gmail.com>
2025-02-09 23:10:22 +0700

    Update penjelasan Factory Constructor

--- bin/factory_constructor.dart
+++ bin/factory_constructor.dart
@@ -3,6 +3,8 @@ class Database {
     print('Database Created');
   }
 
+// iniDatabase adlh variabel yg menyimpan objek
+// Database() adalah objek dari kelas Database.
   static Database iniDatabase = Database();
 
 // Kalo mau ngatur return valuenya, gunakan Factory
@@ -17,15 +19,21 @@ void main() {
   var nofac1 = Database();
   var nofac2 = Database();
 
-// Dengan Factory.
-//walaupun 2x panggil tapi data yg sama.
+/*
+Dengan Factory.
+Hanya mencetak 1x, Karena :
+  1. Pas pertama dipanggil, mengembalikan objek yg udah dibuat sebelumnya ( yg statis ).
+
+  2. Pas kedua kali dipanggil, mengembalikan objek yg sama yaitu iniDatabase ( TIDAK MEMBUAT OBJEK BARU).
+*/
   var cek1 = Database.get();
   var cek2 = Database.get();
 
   print(nofac1 == nofac2); // false
 
 /* true, 
-krn data yg sama, 
-yaitu variabel iniDatabase*/
+krn cek1 dan cek2 mengacu pada objek yg sama,
+yaitu iniDatabase
+*/
   print(cek1 == cek2); // true
 }
