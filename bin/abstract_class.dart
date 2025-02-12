2a5221b979dd8cb3ae41e285f126310507cd7c1c
ukasyaaah <ukasyaaah@gmail.com>
2025-02-12 07:39:49 +0700

    lanjut

--- bin/abstract_class.dart
+++ bin/abstract_class.dart
@@ -1,11 +1,12 @@
-// Abstract class tidak bisa dibuat sebagai object secara langsung, 
+// Abstract class tidak bisa dibuat sebagai object secara langsung,
 
 //hanya bisa diturunkan (cuma bisa jadi parent class).
 
 // Didalam block abstract class, boleh menambah field, method, constructor (Bebas, sama aja).
 
-// Yg membedakan, dia tidak bisa dibikin objectnya secara langsung. 
+// Yg membedakan, dia tidak bisa dibikin objectnya secara langsung.
 
+// Jika bikin objectnya, maka akan error
 
 abstract class Location {
   String? name;
@@ -15,4 +16,12 @@ class City extends Location {
   City(String name) {
     this.name = name;
   }
+}
+
+void main() {
+  // Jika bikin objectnya, maka akan error
+  // var location = Location(); // Error
+
+// HAnya bisa bikin object turunannya.
+  var city = City('Semarang');
 }
