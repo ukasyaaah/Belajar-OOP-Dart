3b116849c4c0722f174a0f5fa63abc31d89c4c29
ukasyaaah <ukasyaaah@gmail.com>
2025-02-14 15:09:00 +0700

    Mixin

---
+++ bin/mixin.dart
@@ -0,0 +1,40 @@
+// Mixin = Copy Paste Code dengan Elegan
+
+// reusable code yg bisa digunakan di class lain tanpa terkendala dengan pewarisan.
+
+mixin Playable {
+  String? name;
+
+  void play() {
+    print('Play $name');
+  }
+}
+
+mixin Stoppable {
+  String? name;
+
+  void stop() {
+    print('Stop $name');
+  }
+}
+
+// SEBELUM MIXIN
+class Video {
+  String? name;
+
+  void play() {
+    print('Play $name');
+  }
+
+  void stop(){
+    print('Stop $name');
+  }
+}
+
+// SETELAH MIXIN
+
+class Audio with Playable, Stoppable{
+
+}
+
+
