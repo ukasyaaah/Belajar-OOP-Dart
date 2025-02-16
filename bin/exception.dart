120a045f0fb7de06831bb2cc453d8680bf7a1c32
ukasyaaah <ukasyaaah@gmail.com>
2025-02-16 20:58:52 +0700

    Finished Exception

--- bin/exception.dart
+++ bin/exception.dart
@@ -6,22 +6,35 @@ class IniException implements Exception {
 
 class Account {
   // Fungsi static : bisa akses tanpa membuat Object
-  static void validate(String email, String pass) {
-    if (email == '') {
-      throw IniException('Email Mu Kosong');
+  static void validate(String username, String pass) {
+    if (username == '') {
+      throw IniException('Username Mu Kosong');
     } else if (pass == '') {
       throw IniException('Password mu kosong');
+    }
+    // Multiple Try-Catch
+    else if (username != 'ukasyaaah') {
+      throw Exception('Login to Ukhasyah\'s Accoun is Failed');
     }
   }
 }
 
 void main() {
   try {
-    Account.validate('', '');
-
+    Account.validate('kasmk', 'eko');
     // catch (hayoeror), untuk nangkep exception yg muncul
     // hayoeror, itu Object dari exceptionnya, bisa dipake untuk print pesan erornya, dan bisa di custom namanya.
   } on IniException catch (pesan) {
     print('Validation Error : ${pesan.message}');
+
+  // Boleh ga nyebut class Exceptionnya
+  // Stack Trace untuk mengetahui letak error
+  } catch (e, stackTrace) {
+    print(e,); 
+    print(stackTrace.toString());
+
+// Block finally selalu di eksekusi, dan ga wajib dieksekusi
+  } finally {
+    print('Program Selesai');
   }
 }
