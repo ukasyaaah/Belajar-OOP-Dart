24b497aaa8ce9601d4c51b519b983b923102db21
ukasyaaah <ukasyaaah@gmail.com>
2025-02-16 21:38:18 +0700

    error

---
+++ bin/error.dart
@@ -0,0 +1,13 @@
+void main() {
+  var list = ['Ukhasyah', 'Zufar', 'Hani'];
+  print(list[10]);
+}
+
+/*
+- Selain Exception, ada juga Error.
+- Beda dengan Exception, **Error adalah jenis kesalahan yang harus dihindari**.
+- Jika terjadi, **lebih baik segera hentikan programnya**.
+- Biasanya **terjadi ketika ada kesalahan kode** pada program kita.
+- Idealnya **ga perlu di tangani try-catch**, tapi diperbaiki code nya.
+- Contoh, kita akses index yang salah di List. 
+*/
